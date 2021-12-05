cc9f36b9e6beba9144f3709a9b17b9a6b1883fff
Rana_Ahmed <foster3399@gmail.com>
2021-12-05 11:41:09 +0100

    fixing page  refresh issue
    remove base path
    
    cleanup
    
    simple page
    
    ads
    
    aa
    
    aa
    
    assad
    
    assad

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import './App.css';
-import Todo from './Todo';
-import Home from './Home';
+import "./App.css";
+import Todo from "./Todo";
+import Home from "./Home";
 
-import { BrowserRouter,Routes, Route, Link } from "react-router-dom";
-
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
- 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/todo" element={<Todo/>}  />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div>
+      aaa1
+      <HashRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/todo" element={<Todo />} />
+          </Routes>
+        </div>
+      </HashRouter>
+    </div>
   );
 }
 
